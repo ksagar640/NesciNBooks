872df5b6eb9d14893813e62885d2a78080487938
pheonix <ksagar640@gmail.com>
2020-08-01 11:38:11 +0530

    update

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-var createError = require('http-errors');
+
 var express = require('express');
-var path = require('path');
-
-var mongoose 				= require('mongoose');
-// const MongoClient = require(‘mongodb’).MongoClient;
-// const uri = "mongodb+srv://admin:<qlzpam22%24>@cluster0-nglwm.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-
-
-var passport 				= require('passport');
-var bodyParser 				= require("body-parser");
-var User					= require("./models/User");
-var LocalStrategy 		   	= require("passport-local");
-var passportLocalMongoose   = require("passport-local-mongoose");
-var book 					= require("./models/book");
-var session 				= require('express-session');
-var expressValidator        = require('express-validator');
-var MongoStore 				= require('connect-mongo')(session);
-
 var app = express();
+var mongoose 				= require('mongoose');
+var passport        = require('passport');
+var bodyParser        = require("body-parser");
+var User          = require("./models/User");
+var LocalStrategy         = require("passport-local");
+var passportLocalMongoose   = require("passport-local-mongoose");
+var book          = require("./models/book");
+var session         = require('express-session');
+var expressValidator        = require('express-validator');
+var MongoStore        = require('connect-mongo')(session);
+const paypal = require('paypal-rest-sdk');
+
+mongoose.connect("mongodb://localhost/Bookdb");
+
+var User          = require("./models/User");
+var book          = require("./models/book");
+var Cart          = require("./models/cart");
+var seed=require("./seed");
+
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(session({
   name: 'session-id',
-  secret: 'this_is_worst_session_secret',
+  secret: 'It does not do well to dwell on dreams and forget to live',
   saveUninitialized: false,
   resave: false,
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  cookie:{ maxAge: 180*60*1000 }
+  cookie:{ maxAge: 7*60*1000 }
 }));
 
+passport.use(new LocalStrategy(User.authenticate()));
 app.use(passport.initialize());
 app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.authenticate('local', { failureFlash: 'Invalid username or password.' });
+passport.authenticate('local', { successFlash: 'Welcome!' });
+//seed();
+
 app.use(function(req,res,next){
   res.locals.currentUser=req.user;
 	res.locals.session = req.session;
@@ -41,40 +51,196 @@ app.use(function(req,res,next){
 });
 
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AS41eR11Fn1sdsuGj1UJahkpfzFfBQIF3rjVg3Z2wArBpZx4r9W_Q9twJDgUNwiz1MTPi7rTwqM-r5GJ',
+  'client_secret': 'EPkbdtKWS640FxNYM2L9YfIhzvh45JE6P-dytD25z1QRIOxF5Oc2K5tC09jdeSydamAKqXrPqbnm5Ejf'
 });
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+var cost = 0;
+
+
+/* GET home page. */
+app.get('/',function(req, res, next) {
+  res.render('index');
 });
 
+app.get('/store',function(req,res){
+  //Collecting ece books..
+  var cart = new Cart (req.session.cart ? req.session.cart : {items:{},totalQty:0,totalPrice:0});
+  req.session.cart = cart;
+  book.find({},function(err,doc){
+    if (err)
+      console.log("NO Book Added to store page");
+    else
+    {
+      res.render('store',{book:doc});
+    }
+  });
+});
 
-//   client.close();
-// });
+app.get("/add-to-cart/:bookid",function(req,res){
+  var cart = new Cart (req.session.cart ? req.session.cart : {items:{},totalQty:0,totalPrice:0});
 
-module.exports = app;
+  book.findOne({id:req.params.bookid},function(err,doc){
+    if (err)
+    {
+      return res.redirect("/store");
+    }
+    cart.add(doc,doc.id);
+    req.session.cart = cart;
+    console.log(cart);
+    res.redirect("/store");
+  });
+});
+
+app.get("/cart",isLoggedIn,function(req,res){
+  if (!req.session.cart)
+  {
+    return res.redirect("/store");
+  }
+  var cart = new Cart(req.session.cart);
+  return res.render("cart",{bookArray: cart.generateArray(),cost: cart.totalPrice});
+});
+
+app.get("/update/:bookid",isLoggedIn,function(req,res){
+  var cart = new Cart(req.session.cart);
+  book.findOne({id:req.params.bookid},function(err,doc){
+          if (err)
+            {
+              console.log(err);
+              return res.redirect("/cart");
+            }
+
+          else
+            {
+              cart.remove(doc,doc.id);
+              console.log(req.session.cart);
+              req.session.cart = cart;
+              
+              res.redirect("/cart");
+            }
+        });
+});
+
+/*  Auth Routes */
+app.get('/signIn',function(req,res){
+   res.render("SignIn");
+});
+
+app.post("/signIn",passport.authenticate('local', 
+  {successRedirect: "/store",failureRedirect: '/signIn'}),((req,res)=>{
+    req.session.username = req.body.username;
+    req.session.password = req.body.password;
+  res.redirect("/store");
+}));
+
+app.get('/signUp',function(req,res){
+  res.render("signUp",{showAlert:null});
+});
+
+app.post('/signUp',function(req,res){
+  User.register(new User({username: req.body.username}),req.body.password,function(err,newuser){
+    if (err)
+    {
+      res.render('signUp',{showAlert:'true'});
+    }
+    console.log(newuser);
+    passport.authenticate("local")(req,res,function(){
+      req.session.username = req.body.username;
+        req.session.password = req.body.password;
+    
+      res.redirect('/store');
+    });
+  });
+});
+
+app.get("/logout", function(req, res){
+   req.session.destroy();    
+     req.logout();    
+     res.redirect("/");
+});
+
+app.post('/pay',function(req,res){
+  cost = req.body.totalcost;
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+   "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Books",
+                "sku": "001",
+                "price": cost,
+                "currency": "rupees",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": cost
+        },
+        "description": "Hat for the best team ever"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      console.log(payment);
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }   
+  }
+});
+});
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+  
+
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "rupees",
+            "total" : cost
+        }
+    }]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+        cost = 0;
+        throw error;
+    } else {
+        console.log(JSON.stringify(payment));
+        res.send('Payment is Successfull and Happy Reading Moments !!!');
+        cost=0;
+    }
+});
+});
+
+app.get('/cancel', (req, res) => res.send('Sorry Payment Failed please pay again'));
+
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/signIn");
+}
+
+app.listen(3000,function () {
+  console.log("server started! at port 3000");
+})
